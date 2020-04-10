40d92ce764c8b8e6178635e5ba1cfb9bfe5bc62b
Kirsten Vermeulen <kirsten.vermeulen@student.howest.be>
2020-04-10 11:16:41 +0200

    Init Project

---
+++ Quiztopia.Models/Data/QuiztopiaDbContext.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace Quiztopia.Models.Data
+{
+    public class QuiztopiaDbContext : IdentityDbContext<IdentityUser>
+    {
+        // Constructor
+
+        public QuiztopiaDbContext(DbContextOptions<QuiztopiaDbContext> options)
+            : base(options)
+        {
+        }
+
+        // Properties
+
+
+        // Fluent API
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            /* modelBuilder.Entity<PersonsEducations>(entity =>
+            {
+                entity.HasKey(e => new { e.PersonId, e.EducationId });
+            }); */
+
+        }
+    }
+}
